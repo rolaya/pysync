ffdb9e48ddabbd9009b71f895bc2e331b82068b4
David Paeme <david@ooyo.be>
2016-03-11 11:05:57 +0100

    Minor update
    Some refactoring and compacting of the code, no new features

--- pysync.py
+++ pysync.py
@@ -9,111 +9,121 @@ import os,filecmp,shutil
 
 # DEFAULTS
 pysyncMajorVersion = 0
-pysyncMinorVersion = 1
+pysyncMinorVersion = 2
 
-# copytree function
-def syncTree (src,dst,delete=False,verbose=False,fake=False):
-	# compare the directories 
-	# remark: we don't touch the hidden files
+# GLOBAL FLAGS
+verbose = False
+fake = False
+delete = False
+
+# copyDir function
+def copyDir (src,dst):
+	# copy the contents of the src directory to the src directory
 	dirCmp = filecmp.dircmp(src,dst)
-	v = verbose
-	# see if we have differences, from origin
+
+	# for files only in origin:
 	for name in dirCmp.left_only:
-		# if it's a hidden file, pass
+		# pass hidden files
 		if not name[:1] is ".": 
-			# if it's a directory, we create it in dst, and launch a syncTree on it
+			# get the full path names
 			srcName = os.path.join(src,name)
 			dstName = os.path.join(dst,name)
+			# if it's a dir, create it and launch the copyDir again
 			if os.path.isdir(srcName):
-				if not fake:
-					try:
+				try:
+					if not fake: 
 						os.makedirs(dstName)
-						if v: print "d+ - %s" % dstName
-					except:
-						print "d# - ERROR: %s can't be created." % dstName
-						continue
-					# a new dir, a new synTree
-					syncTree(srcName,dstName,verbose=v,delete=delete,fake=fake)
-
-			else: 
-				# we have a file
-				if not fake:
-					try:
+						copyDir(srcName,dstName)
+					if verbose: print "d+ %s\t->\t%s" % (srcName,dstName)
+				except:
+					print "ERROR: can't create or copy directory %s" % dstName
+					continue
+			# if it's a file, copy the thing and sync the stats
+			else:
+				try:
+					if not fake:
 						shutil.copy2(srcName,dstName)
 						shutil.copystat(srcName,dstName)
-					except:
-						print "f# - ERROR: %s can't be written." % dstName
-						continue
-				if v: print "f+ - %s ---> %s" % (srcName,dstName)
+					if verbose: print "c+ %s\t->\t%s " % (srcName,dstName)
+				except:
+					print "ERROR: can't copy file %s to %s" (srcName,dstName)
+					continue
 
-	# for the directories we had common, launch the syncTree
+	# if we have common directories, launch the copyDir immediately
 	for name in dirCmp.common_dirs:
-		if not name[:1] is ".":
-			syncTree(os.path.join(src,name),os.path.join(dst,name),verbose=v,delete=delete,fake=fake)
-
-	# for the common files, we compare the stats (date)
-	for name in dirCmp.common_files:
 		if not name[:1] is ".": 
+			if not fake: copyDir(os.path.join(src,name),os.path.join(dst,name))
+
+	# if we have files in common, check the source en destination stats
+	for name in dirCmp.common_files:
+		if not name[:1] is ".":
 			srcName = os.path.join(src,name)
 			dstName = os.path.join(dst,name)
-			# compare the stats
 			if not filecmp.cmp(srcName,dstName):
-				# files are not the same, we copy the src to dst
-				if not fake:
-					try:
+				try:
+					if not fake:
 						os.remove(dstName)
 						shutil.copy2(srcName,dstName)
-						shutil.copystat(srcName,dstName)
-					except:
-						print "f# - ERROR: %s can't be updated" % dstName
-						continue
-					if v: print "fu - %s ---> %s" % (srcName,dstName)
+						shutil.copystats(srcName,dstName)
+					if verbose: print "u= %s" % dstName
+				except:
+					print "ERROR: can't update file %s to %s" (srcName,dstName)
+					continue
 
-
-	# for the directories and files only existing in the destination, if the complete flag is set
+	# if we have stuff in the dst dir that isn't in the src dir, we delete
 	if delete:
 		for name in dirCmp.right_only:
-			if not name[:1] is ".": 
+			if not name[:1] is ".":
 				dstName = os.path.join(dst,name)
 				if os.path.isdir(dstName):
-					if not fake:
-						try:
-							shutil.rmtree(dstName)
-						except:
-							print "d# - ERROR: %s can't be removed." % dstName
-							continue
-					if v: print "d- ---x %s" % dstName
+					try:
+						if not fake: shutil.rmtree(dstName)
+						if verbose: print "x- %s" % dstName
+					except:
+						print "ERROR: can't delete directory %s" % dstName
 				else:
-					if not fake: 
-						try:
-							os.remove(dstName)
-						except:
-							print "d# - ERROR: %s can't be removed." % dstName
-					if v: print "f- ---x %s" % dstName
+					try:
+						if not fake: os.remove(dstName)
+						if verbose: print "x- %s" % dstName
+					except:
+						print "ERROR: can't delete file %s" % dstFile
+
 
 
 
 # Argparser
-argParser = argparse.ArgumentParser(description="pysync.py v%d.%d-- Syncs the file tree of two directories." % (pysyncMajorVersion,pysyncMinorVersion))
-parseGroup = argParser.add_mutually_exclusive_group(required=True)
-parseGroup.add_argument("-s","--sync",action="store_true",help="Standard sync, origin overwrites destination, deletes surplus files at destination")
-parseGroup.add_argument("-c","--complete",action="store_true",help="Completion sync, origin is added to destination, no files are deleted in destination.")
-argParser.add_argument("-v","--verbose",action="store_true",help="Log all actions taken by the script to the command line.")
-argParser.add_argument("-f","--fake",action="store_true",help="Only simulate the actions. Use this with the --verbose option.")
-argParser.add_argument("origin",help="The origin directory.")
-argParser.add_argument("destination",help="The destination directory.")
-args = argParser.parse_args()
+def main():
+	global verbose
+	global fake
+	global delete
 
-# Check if the directories exist and if the destination is writable
-if not os.path.exists(args.origin):
-	print "Origin directory does not exist."
-	sys.exit(1)
-if not os.path.exists(args.destination):
-	print "Destination directory does not exist."
-	sys.exit(1)
-elif not os.access(args.destination,os.W_OK):
-	print "Destination directory is not writable"
-	sys.exit(2)
+	argParser = argparse.ArgumentParser(description="pysync.py v%d.%d-- Syncs the file tree of two directories." % (pysyncMajorVersion,pysyncMinorVersion))
+	parseGroup = argParser.add_mutually_exclusive_group(required=True)
+	parseGroup.add_argument("-s","--sync",action="store_true",help="Standard sync, origin overwrites destination, deletes surplus files at destination")
+	parseGroup.add_argument("-c","--complete",action="store_true",help="Completion sync, origin is added to destination, no files are deleted in destination.")
+	argParser.add_argument("-v","--verbose",action="store_true",help="Log all actions taken by the script to the command line.")
+	argParser.add_argument("-f","--fake",action="store_true",help="Only simulate the actions. Use this with the --verbose option.")
+	argParser.add_argument("origin",help="The origin directory.")
+	argParser.add_argument("destination",help="The destination directory.")
+	args = argParser.parse_args()
 
-# let's sync the tree
-syncTree(args.origin,args.destination,verbose=args.verbose,delete=args.sync,fake=args.fake)
+	# Check if the directories exist and if the destination is writable
+	if not os.path.exists(args.origin):
+		print "Origin directory does not exist."
+		sys.exit(1)
+	if not os.path.exists(args.destination):
+		print "Destination directory does not exist."
+		sys.exit(1)
+	elif not os.access(args.destination,os.W_OK):
+		print "Destination directory is not writable"
+		sys.exit(2)
+
+	# let's set the globals and sync the tree
+	verbose = args.verbose
+	delete = args.sync
+	fake = args.fake
+	#print "verbose = %s\t\tdelete = %s\t\tfake=%s" % (verbose,delete,fake)
+	copyDir(args.origin,args.destination)
+
+if __name__ == "__main__":
+	main()
