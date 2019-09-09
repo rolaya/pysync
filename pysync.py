5c4b2df03e44d608d19fa8ea76e79a17633feefa
Raul Olaya <raul.olaya@gmail.com>
2019-09-09 10:43:38 -0400

    1. Modifications to copy hidden files as per user parameters (i.e. supply -a). Defaults to NOT copying hidden files. 2. Misc. modifications to prevent exceptions (due to python versions). 3. Added misc. debug messages (controlled via global "debug" variable). Defaults to NOT output debug messages.

--- pysync.py
+++ pysync.py
@@ -15,6 +15,10 @@ pysyncMinorVersion = 2
 verbose = False
 fake = False
 delete = False
+debug = False
+
+# By default, exclude hidden files from copy process
+copy_all_files = False
 
 # copyDir function
 def copyDir (src,dst):
@@ -23,8 +27,12 @@ def copyDir (src,dst):
 
 	# for files only in origin:
 	for name in dirCmp.left_only:
+
+		if debug:
+			print("File unique to source directory: [%s]" % name)
+
 		# pass hidden files
-		if not name[:1] is ".": 
+		if copy_all_files or not name[:1] is ".": 
 			# get the full path names
 			srcName = os.path.join(src,name)
 			dstName = os.path.join(dst,name)
@@ -51,21 +59,33 @@ def copyDir (src,dst):
 
 	# if we have common directories, launch the copyDir immediately
 	for name in dirCmp.common_dirs:
-		if not name[:1] is ".": 
+
+		if debug:
+			print("Common directory: [%s]" % name)
+
+		if copy_all_files or not name[:1] is ".": 
 			if not fake: copyDir(os.path.join(src,name),os.path.join(dst,name))
 
 	# if we have files in common, check the source en destination stats
 	for name in dirCmp.common_files:
-		if not name[:1] is ".":
+		
+		if debug:
+			print("Common file: [%s]" % name)
+		
+		if copy_all_files or not name[:1] is ".":
 			srcName = os.path.join(src,name)
 			dstName = os.path.join(dst,name)
 			if not filecmp.cmp(srcName,dstName):
 				try:
 					if not fake:
+
+						# The file's contents have changed, replace old file and update its stats.
 						os.remove(dstName)
 						shutil.copy2(srcName,dstName)
-						shutil.copystats(srcName,dstName)
-					if verbose: print "u= %s" % dstName
+						shutil.copystat(srcName,dstName)
+
+					if verbose: 
+						print "u= %s" % dstName
 				except:
 					print "ERROR: can't update file %s to %s" (srcName,dstName)
 					continue
@@ -73,7 +93,7 @@ def copyDir (src,dst):
 	# if we have stuff in the dst dir that isn't in the src dir, we delete
 	if delete:
 		for name in dirCmp.right_only:
-			if not name[:1] is ".":
+			if copy_all_files or not name[:1] is ".":
 				dstName = os.path.join(dst,name)
 				if os.path.isdir(dstName):
 					try:
@@ -96,11 +116,19 @@ def main():
 	global verbose
 	global fake
 	global delete
+	global copy_all_files
+
+	# Set to True to get some additional process flow debug messages.
+	global debug
 
 	argParser = argparse.ArgumentParser(description="pysync.py v%d.%d-- Syncs the file tree of two directories." % (pysyncMajorVersion,pysyncMinorVersion))
+	
 	parseGroup = argParser.add_mutually_exclusive_group(required=True)
+	
 	parseGroup.add_argument("-s","--sync",action="store_true",help="Standard sync, origin overwrites destination, deletes surplus files at destination")
 	parseGroup.add_argument("-c","--complete",action="store_true",help="Completion sync, origin is added to destination, no files are deleted in destination.")
+	
+	argParser.add_argument("-a","--copy_all_files",action="store_true",help="Copy all files (including hidden files)")
 	argParser.add_argument("-v","--verbose",action="store_true",help="Log all actions taken by the script to the command line.")
 	argParser.add_argument("-f","--fake",action="store_true",help="Only simulate the actions. Use this with the --verbose option.")
 	argParser.add_argument("origin",help="The origin directory.")
@@ -122,7 +150,16 @@ def main():
 	verbose = args.verbose
 	delete = args.sync
 	fake = args.fake
-	#print "verbose = %s\t\tdelete = %s\t\tfake=%s" % (verbose,delete,fake)
+
+	# If -or --copy_all_files specified, hidden files will be copied as well.
+	copy_all_files = args.copy_all_files
+
+	if debug:
+		print "Copy all files (including hidden files): [%s]" % (copy_all_files)
+		print "verbose: [%s]" % (verbose)
+		print "delete:  [%s]" % (delete)
+		print "fake:    [%s]" % (fake)
+
 	copyDir(args.origin,args.destination)
 
 if __name__ == "__main__":
